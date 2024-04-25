67ce036caa57f88ddeaea72444c3e4518a09fe4a
Benlojgo1 <benlojgo3@gmail.com>
2024-04-25 17:42:14 -0500

    commmmmitttt

--- src/main/java/com/example/semesterproject/HelloApplication.java
+++ src/main/java/com/example/semesterproject/HelloApplication.java
@@ -11,6 +11,7 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.lang.module.FindException;
 
 public class HelloApplication extends Application {
     @Override
