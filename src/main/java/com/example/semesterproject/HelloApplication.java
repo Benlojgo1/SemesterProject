e681c0d93ca0306f47c353b5d86c59ebf4db98e0
Orange-Juice03 <160554493+Orange-Juice03@users.noreply.github.com>
2024-04-30 17:26:05 -0500

    Changed the grid still reveals flag,bomb, and numbers. Looks solid

--- src/main/java/com/example/semesterproject/HelloApplication.java
+++ src/main/java/com/example/semesterproject/HelloApplication.java
@@ -1,44 +1,87 @@
 package com.example.semesterproject;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.awt.*;
+import java.io.IOException;
 
 public class HelloApplication extends Application {
-
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage stage) throws IOException {
+        int mineCount = 0;
         // Create the GridPane for button layout
         GridPane gridPane = new GridPane();
         gridPane.setAlignment(Pos.CENTER);
-
+        Button[][] buttons = new MineButton[8][8];
         // Create buttons and add them to the grid
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                Button button = new Button();
-
+                buttons[i][j] = new MineButton(i, j);
+                /** generates mines*/
+                ((MineButton)buttons[i][j]).generateMine();
+                if (((MineButton)buttons[i][j]).hasMine()) {
+                    mineCount++;
+                }
                 /** Set button size */
-                button.setPrefSize(50, 50);
+                buttons[i][j].setPrefSize(50, 50);
 
                 // Add button to the gridpane at specific row and column
-                gridPane.add(button,i, j);
+                gridPane.add(buttons[i][j], i, j);
             }
         }
 
-        // Set the scene with the gridPane and title
-        Scene scene = new Scene(gridPane);
-        primaryStage.setTitle("Minesweeper");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                ((MineButton) buttons[i][j]).checkMine((MineButton[][]) buttons);
+            }
+        }
+
+        ClickHandler clickHandler = new ClickHandler();
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                buttons[i][j].setOnMouseClicked(clickHandler);
+            }
+        }
+
+        HBox title = new HBox();
+        title.getChildren().add(new Label(String.valueOf(mineCount)));
+        title.setAlignment(Pos.CENTER);
+        VBox pane = new VBox();
+        pane.getChildren().addAll(title, gridPane);
+        Scene scene = new Scene(pane);
+        stage.setTitle("Minesweeper");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    class ClickHandler implements EventHandler<MouseEvent> {
+        @Override
+        public void handle(MouseEvent e) {
+            if (e.getButton() == MouseButton.SECONDARY) {
+                ((MineButton) e.getSource()).setFlagText();
+            }
+            else {
+                if (((MineButton) e.getSource()).hasMine()) {
+                    ((MineButton) e.getSource()).setLoseText();
+                } else {
+                    ((MineButton) e.getSource()).setText();
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
 }
+
--- src/main/java/com/example/semesterproject/MineButton.java
+++ src/main/java/com/example/semesterproject/MineButton.java
@@ -27,24 +27,29 @@ public class MineButton extends Button {
         }
     }
 
-    public int checkMine(MineButton[][] buttons) {
-        int count = 0;
+    public void checkMine(MineButton[][] buttons) {
+
         int row = buttons.length;
         int column = buttons.length;
-
         for (int i = x - 1; i <= x + 1; i++) {
             for (int j = y - 1; j <= y + 1; j++) {
-                if (i > -1 && i < 8 && j > -1 && j < 8) {
+                if (i > -1 && i < row && j > -1 && j < column) {
                     if (buttons[i][j].hasMine()) {
-                        count++;
+                        surroundMine++;
                     }
                 }
             }
         }
-        return count;
+    }
+
+    public void setFlagText() {
+        setText("🚩");
+    }
+    public void setLoseText() {
+        setText("💣");
     }
 
     public void setText() {
-
+        setText(String.valueOf(surroundMine));
     }
 }
